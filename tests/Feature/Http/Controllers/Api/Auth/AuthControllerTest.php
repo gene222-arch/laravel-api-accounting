01700c0af8e3815fb61c446a99cdf81e2895af38
unknown <genephillip222@gmail.com>
2021-04-01 20:50:56 +0800

    initial release

---
+++ tests/Feature/Http/Controllers/Api/Auth/AuthControllerTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Feature\Http\Controllers\Api\Auth;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class AuthControllerTest extends TestCase
+{
+
+    /** @test */
+    public function user_can_view_authenticated_user()
+    {
+        $response = $this->get(
+            '/api/auth',
+            $this->apiHeader()
+        );
+
+        $this->assertResponse($response);
+    }
+
+
+}
